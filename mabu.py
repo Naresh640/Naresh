508a7f9a242c4ec0919772f2deb5020ff6d29a8c
Naresh640 <narah6k@gmail.com>
2026-08-21 12:47:55 +0530

    Add files via upload

---
+++ mabu.py
@@ -0,0 +1,25 @@
+'''text = "Artificalintelligence"
+
+count = len(text)
+
+print("Number of charactter:", count)'''
+
+
+'''num1 = int(input("Enter first number:"))
+num2 = int(input("Enter first number:"))
+
+sum = num1 + num2
+
+print("Sum =", sum)'''
+
+
+''list = [10, 20, 30, 40]
+
+my_list.insert(2,25)
+print("After insert():", my_list)
+
+my_list.append(50)
+print("After append():", my_list)
+
+my_list.remove(20)
+
